2bd1aff6dee2b2f4918258d94ca95072c51289f5
nevesfg <jaojose711@gmail.com>
2023-10-02 16:01:14 -0300

    Initial commit

---
+++ app/src/main/java/com/nevesfg/projetos/aula2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nevesfg.projetos.aula2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage_relative_activity);
+    }
+}
